f3f42411f2fe79f38776b2626058d4ea0ae77961
Ste <32852521+StefanGunnlaugur@users.noreply.github.com>
2018-02-17 03:51:58 +0000

    Fix port

--- app.js
+++ app.js
@@ -129,7 +129,7 @@ app.use((err, req, res) => {
 });
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, hostname, () => {
   console.info(`Server running at http://${hostname}:${port}/`);
